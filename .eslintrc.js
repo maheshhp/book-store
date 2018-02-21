040c1510ecd0dbee1ad0e4ae116bf2a4dd297953
maheshhp <hpmahesh73@gmail.com>
2018-02-21 10:59:30 +0530

    Initial react app setup

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "babel-eslint",
+  "extends": "airbnb",
+  "rules": {
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "react/forbid-prop-types": 0,
+  }
+};
